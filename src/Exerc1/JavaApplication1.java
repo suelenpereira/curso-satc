e7f828f15b5e2872fbd054c5ef534278b7b33e53
EDUTEC <EDUTEC@10.8.0.3>
2013-06-15 10:08:31 -0300

    inicio

--- src/Exerc1/JavaApplication1.java
+++ src/Exerc1/JavaApplication1.java
@@ -1,19 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exerc1;
 
-/**
- *
- * @author EDUTEC
- */
-public class JavaApplication1 {
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    import java.io.IOException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+public class JavaApplication1 {
+   
+   public static void main(String[] args) {    
+      BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+      String nota1="",nota2="",nota3="";
+      Double soma=0.;      
+      for (int i=1;i<=3;i++){
+        System.out.print("Entre com a nota: ");       
+        try{
+            nota1=dataIn.readLine();          
+           } catch (IOException e) {
+        System.out.println("Error!");
+        }
+       soma=soma+Double.valueOf(nota1);
+      }
+      soma=soma/3;
+      String carinha;
+      if (soma>6)
+          carinha=":-)";
+      else
+          carinha=":-(";
+      System.out.println("MEDIA: "+soma+"  "+carinha+"!");
+      
+   }
+    
 }
